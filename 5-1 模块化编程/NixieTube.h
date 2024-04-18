7702060465ab889ef9e282a5e39a2f9c6cc8710d
SunshineJiaWei <251500892@qq.com>
2024-04-18 15:24:26 +0800

    refactor: modularization management

---
+++ 5-1 模块化编程/NixieTube.h
@@ -0,0 +1,7 @@
+
+#ifndef __NIXIETUBE_H__
+#define __NIXIETUBE_H__
+
+void NixieTube(unsigned char location, unsigned char num);
+
+#endif
---
+++ 5-1 模块化编程/main.c
@@ -0,0 +1,19 @@
+#include <REGX52.H>
+#include "NixieTube.h"
+
+void main()
+{
+	while(1)
+	{
+		NixieTube(1,1);
+		NixieTube(2,2);
+		NixieTube(3,3);
+		NixieTube(4,4);
+		NixieTube(5,5);
+		NixieTube(6,6);
+		NixieTube(7,7);
+		NixieTube(8,8);
+
+	}
+
+}
